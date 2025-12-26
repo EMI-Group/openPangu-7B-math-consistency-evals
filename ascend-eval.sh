f1690e6ab581ce2b953a9744e9e16af9345789d6
zaber <20830726+ZaberKo@users.noreply.github.com>
2025-12-26 13:25:24 +0800

    update req.txt and scripts

--- ascend-eval.sh
+++ ascend-eval.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 set -ex
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3
+# export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3
 export VLLM_USE_V1=1
+
+
 
 ############## SimpleRL ##############
 # DATA_NAME="amc23,var_amc23,aime24,var_aime24,aime25,var_aime25"
-DATA_NAME="amc23"
+DATA_NAME=${1:-"amc23,var_amc23,aime24,var_aime24,aime25,var_aime25"}
+N_SAMPLES=${2:-"1"}
 
 python3 -u math_eval.py \
         --model_name_or_path /opt/pangu/openPangu-Embedded-7B-V1.1 \
         --data_name $DATA_NAME \
-        --output_dir "score_pass_16/openPangu-Embedded-7B-V1.1" \
+        --output_dir "score_pass_${N_SAMPLES}/openPangu-Embedded-7B-V1.1" \
         --prompt_type "pangu" \
         --num_test_sample "-1" \
         --temperature 0.6 \
-        --n_sampling 16 \
+        --n_sampling ${N_SAMPLES} \
         --top_p 1 \
         --use_vllm \
         --max_tokens_per_call 8192 \
--- eval.sh
+++ eval.sh
@@ -1,26 +1,25 @@
 set -ex
-export CUDA_VISIBLE_DEVICES=0,1
+# export CUDA_VISIBLE_DEVICES=0,1
 
 ############## SimpleRL ##############
-MODEL_NAME_OR_PATH_LIST=(Qwen/Qwen2.5-MATH-7B)
-PROMPT_TYPE="qwen25-math-cot"
+# MODEL_NAME_OR_PATH=Qwen/Qwen2.5-MATH-7B
+# PROMPT_TYPE="qwen25-math-cot"
+MODEL_NAME_OR_PATH=FreedomIntelligence/openPangu-Embedded-7B-V1.1
+PROMPT_TYPE="pangu"
 
-DATA_NAME="amc23,var_amc23,aime24,var_aime24,aime25,var_aime25"
+DATA_NAME=${1:-"amc23,var_amc23,aime24,var_aime24,aime25,var_aime25"}
+N_SAMPLES=${2:-"1"}
 
-for MODEL_NAME_OR_PATH in "${MODEL_NAME_OR_PATH_LIST[@]}"
-do
-    TOKENIZERS_PARALLELISM=false \
-    python3 -u math_eval.py \
-        --model_name_or_path ${MODEL_NAME_OR_PATH} \
+python3 -u math_eval.py \
+        --model_name_or_path ${} \
         --data_name $DATA_NAME \
-        --output_dir "score_pass_1/${MODEL_NAME_OR_PATH}" \
-        --prompt_type $PROMPT_TYPE \
+        --output_dir "score_pass_${N_SAMPLES}/openPangu-Embedded-7B-V1.1" \
+        --prompt_type "pangu" \
         --num_test_sample "-1" \
         --temperature 0.6 \
-        --n_sampling 16 \
+        --n_sampling ${N_SAMPLES} \
         --top_p 1 \
         --use_vllm \
         --max_tokens_per_call 8192 \
-        # --apply_chat_template \
+        --apply_chat_template \
         --save_outputs
-done
